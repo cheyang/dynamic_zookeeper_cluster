2391115908989ff9e9310ddcd606b7535c0082d8
cheyang <cheyang@163.com>
2015-11-02 18:33:21 +0800

    strings

--- zookeeper_deployer/src/deployer/common.go
+++ zookeeper_deployer/src/deployer/common.go
@@ -4,10 +4,16 @@ import (
 	"os/exec"
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 
 func Exec(cmd string, args []string) (output string, err error){
+	
+	fmt.Print(cmd)
+	
+	fmt.Println(strings.Join(args, " "))
+	
 	command := exec.Command(cmd, args...)
 	
 	
