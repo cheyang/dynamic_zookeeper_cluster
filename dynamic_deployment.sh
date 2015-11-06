3520d7151f5c6c4bbc146a00872eb03058a04b4b
cheyang <cheyang@163.com>
2015-11-06 16:37:46 +0800

    remove zk-init.sh

--- dynamic_deployment.sh
+++ dynamic_deployment.sh
@@ -6,13 +6,13 @@ if [ -n $ZK ]; then
      echo "`bin/zkCli.sh -server $ZK:$MYCLIENT_PORT get /zookeeper/config|grep ^server`" >> ${ZOO_CFG}.dynamic
      echo "server.$MYID=$MYSERVER_URL:observer;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
      cp ${ZOO_CFG}.dynamic  ${ZOO_CFG}.dynamic.org
-     ${ZOO_DIR}/bin/zkServer-initialize.sh --force --myid=$MYID
+     echo "${MYID}" > /tmp/zookeeper/myid
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start
      ${ZOO_DIR}/bin/zkCli.sh -server $ZK:$MYCLIENT_PORT reconfig -add "server.$MYID=$MYSERVER_URL:participant;$MYCLIENT_PORT"
      ${ZOO_DIR}/bin/zkServer.sh stop
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground  
 else
      echo "server.$MYID=$MYSERVER_URL:participant;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
-     ${ZOO_DIR}/bin/zkServer-initialize.sh --force --myid=$MYID
+     echo "${MYID}" > /tmp/zookeeper/myid
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground
 fi
