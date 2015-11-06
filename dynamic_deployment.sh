b210017eeaf11eb55473243a6e71cfa4827ea5d6
cheyang <cheyang@163.com>
2015-11-06 20:52:52 +0800

    correct env name

--- dynamic_deployment.sh
+++ dynamic_deployment.sh
@@ -2,17 +2,17 @@
 ZOO_DIR="/opt/zookeeper"
 ZOO_CFG="${ZOO_DIR}/conf/zoo.cfg"
 
-if [ -n $ZK ]; then 
-     echo "`bin/zkCli.sh -server $ZK:$MYCLIENT_PORT get /zookeeper/config|grep ^server`" >> ${ZOO_CFG}.dynamic
-     echo "server.$MYID=$MYSERVER_URL:observer;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
+if [ -n $ZK_LEADER_URL ]; then 
+     echo "`bin/zkCli.sh -server $ZK_LEADER_URL get /zookeeper/config|grep ^server`" >> ${ZOO_CFG}.dynamic
+     echo "server.$MY_ID=$MY_SERVER_URL:observer;$MY_CLIENT_PORT" >> ${ZOO_CFG}.dynamic
      cp ${ZOO_CFG}.dynamic  ${ZOO_CFG}.dynamic.org
-     echo "${MYID}" > /tmp/zookeeper/myid
+     echo "${MY_ID}" > /tmp/zookeeper/MY_ID
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start
-     ${ZOO_DIR}/bin/zkCli.sh -server $ZK:$MYCLIENT_PORT reconfig -add "server.$MYID=$MYSERVER_URL:participant;$MYCLIENT_PORT"
+     ${ZOO_DIR}/bin/zkCli.sh -server $ZK:$MYCLIENT_PORT reconfig -add "server.$MY_ID=$MY_SERVER_URL:participant;$MY_CLIENT_PORT"
      ${ZOO_DIR}/bin/zkServer.sh stop
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground  
 else
-     echo "server.$MYID=$MYSERVER_URL:participant;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
-     echo "${MYID}" > /tmp/zookeeper/myid
+     echo "server.$MY_ID=$MY_SERVER_URL:participant;$MY_CLIENT_PORT" >> ${ZOO_CFG}.dynamic
+     echo "${MY_ID}" > /tmp/zookeeper/MY_ID
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground
 fi
