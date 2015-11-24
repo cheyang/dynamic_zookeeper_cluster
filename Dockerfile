3dc634406f9cd509d2ca07910a7192d0534ed0c2
cheyang <cheyang@163.com>
2015-11-24 17:23:34 +0800

    zookeeper 3.5.1

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM vixns/java8
 
-RUN wget -q -O - http://apache.arvixe.com/zookeeper/zookeeper-3.5.0-alpha/zookeeper-3.5.0-alpha.tar.gz | tar -xzf - -C /opt \
-    && mv /opt/zookeeper-3.5.0-alpha /opt/zookeeper \
+RUN wget -q -O - http://apache.arvixe.com/zookeeper/zookeeper-3.5.1-alpha/zookeeper-3.5.1-alpha.tar.gz | tar -xzf - -C /opt \
+    && mv /opt/zookeeper-3.5.1-alpha /opt/zookeeper \
     && cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg \
     && mkdir -p /tmp/zookeeper
 
 RUN cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg \
-    && echo "standaloneEnabled=false" >> /opt/zookeeper/conf/zoo.cfg 
- #   && echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg 
+    && echo "standaloneEnabled=false" >> /opt/zookeeper/conf/zoo.cfg  \
+    && echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg 
 
 ADD . /usr/local/bin/
 
--- dynamic_config.sh
+++ dynamic_config.sh
@@ -3,7 +3,6 @@ ZOO_DIR="/opt/zookeeper"
 ZOO_CFG="${ZOO_DIR}/conf/zoo.cfg"
 
 if [ -n "$ZK_URL" ]; then
-     echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg
      echo "`${ZOO_DIR}/bin/zkCli.sh -server $ZK_URL get /zookeeper/config|grep ^server`" >> ${ZOO_CFG}.dynamic
      echo "server.$MYID=$MYSERVER_URL:observer;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
      cp ${ZOO_CFG}.dynamic ${ZOO_CFG}.dynamic.org 
@@ -13,7 +12,6 @@ if [ -n "$ZK_URL" ]; then
      ${ZOO_DIR}/bin/zkServer.sh stop
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground  
 else
-     echo "dynamicConfigFile=/opt/zookeeper/conf/zoo.cfg.dynamic" >> /opt/zookeeper/conf/zoo.cfg
      echo "server.$MYID=$MYSERVER_URL:participant;$MYCLIENT_PORT" >> ${ZOO_CFG}.dynamic
      echo "${MYID}" > /tmp/zookeeper/myid
      ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' ${ZOO_DIR}/bin/zkServer.sh start-foreground
